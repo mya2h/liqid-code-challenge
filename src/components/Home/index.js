c2dc0d03c061e18b7e1d25e8d457182f9121c44c
Kalikidan Mesfin <mesfinkalkidan@gmail.com>
2021-07-26 17:25:16 +0300

    add togle button on mobile view

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,14 +2,15 @@ import Nav from '../Nav'
 import Layout from './landing'
 import ProductChart from './products'
 import Footer from '../Footer'
+import { useState } from 'react'
 
 const Home = () => {
-
+    const [toggle, setToggle] = useState(false)
+    console.log(toggle)
     return (
         <div>
-            <Nav />
+            <Nav setToggle={setToggle} />
             <Layout />
-            <ProductChart />
             <Footer />
         </div>
     )
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,45 +1,33 @@
-import { useState } from 'react'
-import { AiOutlineHome } from 'react-icons/ai'
+import { useState, useRef } from 'react'
+import { AiOutlineHome, AiOutlineClose } from 'react-icons/ai'
 import { BsPerson } from 'react-icons/bs'
 import { FiSettings } from 'react-icons/fi'
 import { GiHamburgerMenu } from 'react-icons/gi'
-import { IoExitOutline } from 'react-icons/io5'
 import mainIcon from '../../assets/images/Group 1.svg'
 import '../../assets/styles/nav.css'
 
 
-const Nav = () => {
+const Nav = ({ setToggle }) => {
     const [navbarOpen, setNavbarOpen] = useState(false)
     const handleToggle = () => {
+        setToggle(!navbarOpen)
         setNavbarOpen(!navbarOpen)
+
     }
     const closeMenu = () => {
         setNavbarOpen(false)
     }
     return (
         <div>
-            <nav class="nav">
-                <div className="toggleButton">
-                    <GiHamburgerMenu onClick={handleToggle} />
-                </div>
-                <div className="nav-title">
-                    <div className="logo">
-                        <img src={mainIcon} />
-                    </div>
-                </div>
-                <div class="nav-left">
-                    <ul>
-                        <li>Hello, Liqid</li>
-                        <li> | </li>
-                        <li><a>   <IoExitOutline className="icon" />Logout</a></li>
-                    </ul>
-                </div>
-                <div class="nav-right">
-                    <ul>
-                        <li><a className="active"><AiOutlineHome className="icon" />Home</a></li>
-                        <li><a><BsPerson className="icon" />Profile</a></li>
-                        <li><a><FiSettings className="icon" />Settings</a></li>
-                    </ul>
+            <nav className={navbarOpen ? "active-nav" : "nav"}>
+                <a href="#" className="nav-logo"> <img src={mainIcon} /></a>
+                <ul className={navbarOpen ? "nav-menu active" : "nav-menu"} >
+                    <li className="nav-item"><a className="active nav-link"><AiOutlineHome className="icon" />Home</a></li>
+                    <li className="nav-item"><a className="nav-link"><BsPerson className="icon" />Profile</a></li>
+                    <li className="nav-item"><a className="nav-link"><FiSettings className="icon" />Settings</a></li>
+                </ul>
+                <div className="humburger" onClick={handleToggle}>
+                    {navbarOpen ? <AiOutlineClose className="toggle-icon" /> : <GiHamburgerMenu className="toggle-icon" />}
                 </div>
             </nav>
         </div>
