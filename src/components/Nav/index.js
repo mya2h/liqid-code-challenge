7d1565181101c30e0ee07bc9b0ff17ec1fc13ed3
kalkidan <mesfinkalkidan@gmail.com>
2021-07-27 17:53:50 +0300

    ui fix for popup menu

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,7 +4,7 @@ import { AiOutlineHome, AiOutlineClose } from 'react-icons/ai'
 import { BsPerson } from 'react-icons/bs'
 import { FiSettings } from 'react-icons/fi'
 import { GiHamburgerMenu } from 'react-icons/gi'
-import {IoLogOutOutline} from 'react-icons/io5'
+import { IoLogOutOutline } from 'react-icons/io5'
 import mainIcon from '../../assets/images/Group 1.svg'
 import redLogo from '../../assets/images/redlogo.svg'
 import '../../assets/styles/nav.css'
@@ -21,20 +21,20 @@ const Nav = ({ setToggle }) => {
         <div>
             <nav className={navbarOpen ? "active-nav" : "nav"}>
                 <div className="nav-title">
-                {navbarOpen?<div className="nav-logo-open"> <img src={redLogo} /></div>:<div className="nav-logo"> <img src={mainIcon} /></div>}
-                <div className="menu-title"><NavLink to="/home" className="nav-link">Hello, Liqid</NavLink></div>
-                <div className="menu-title separator">|</div>
-                <div className="nav-item left-item"><NavLink to="/home" className="nav-link"><IoLogOutOutline className="icon" />Logout</NavLink></div>
+                    {navbarOpen ? <div className="nav-logo-open"> <img src={redLogo} /></div> : <div className="nav-logo"> <img src={mainIcon} /></div>}
+                    <div className="menu-title"><NavLink to="/home" activeClassName="active-link" className="nav-link">Hello, Liqid</NavLink></div>
+                    <div className="menu-title separator">|</div>
+                    <div className="nav-item left-item"><NavLink to="/home" activeClassName="active-link" className="nav-link"><IoLogOutOutline className="icon" />Logout</NavLink></div>
                 </div>
-              
+
                 <div className={navbarOpen ? "nav-menu active" : "nav-menu"}>
-                <ul className="nav-menu-right">
-                    <li className="nav-item right-item"><NavLink to="/home" className="nav-link" ><IoLogOutOutline className="icon" />Logout</NavLink></li>
-                    <li className="nav-item"><NavLink to="/home" className="nav-link" ><AiOutlineHome className="icon" />Home</NavLink></li>
-                    <li className="nav-item"><NavLink to="/home" className="nav-link" ><BsPerson className="icon" />Profile</NavLink></li>
-                    <li className="nav-item"><NavLink to="/home" className="nav-link" ><FiSettings className="icon" />Settings</NavLink></li>
-                </ul>
-                <h2 >LIQID</h2>
+                    <ul className="nav-menu-right">
+                        <li className="nav-item right-item"><NavLink to="/" className="nav-link" ><IoLogOutOutline className="icon" />Logout</NavLink></li>
+                        <li className="nav-item"><NavLink to="/home" activeClassName="active"  className="nav-link" ><AiOutlineHome className="icon" />Home</NavLink></li>
+                        <li className="nav-item"><NavLink to="/" className="nav-link" ><BsPerson className="icon" />Profile</NavLink></li>
+                        <li className="nav-item"><NavLink to="/" className="nav-link" ><FiSettings className="icon" />Settings</NavLink></li>
+                    </ul>
+                    <div className="footer-sidebar">LIQID</div>
                 </div>
                 <div className="hamburger" onClick={handleToggle}>
                     {navbarOpen ? <AiOutlineClose className="toggle-icon" /> : <GiHamburgerMenu className="toggle-icon" />}
