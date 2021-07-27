7ed6f8fe4aa09196ee48506a7d61345aaf69ba77
kalkidan <mesfinkalkidan@gmail.com>
2021-07-27 07:21:25 +0300

    add react router for links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import Home from './components/Home'
+import { BrowserRouter as Router, Switch,Route, Redirect} from "react-router-dom";
 
+import Home from './components/Home'
 function App() {
   return (
     <div>
-     
-    <Home/>
+      <Router>
+       <Switch>
+       <Route path="/home">
+            <Home />
+          </Route>
+          <Redirect from ="/" to ="/home"/>
+        </Switch>
+        </Router>
     </div>
   );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,10 +1,12 @@
-import { useState, useRef } from 'react'
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 import { AiOutlineHome, AiOutlineClose } from 'react-icons/ai'
 import { BsPerson } from 'react-icons/bs'
 import { FiSettings } from 'react-icons/fi'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import {IoLogOutOutline} from 'react-icons/io5'
 import mainIcon from '../../assets/images/Group 1.svg'
+import redLogo from '../../assets/images/redlogo.svg'
 import '../../assets/styles/nav.css'
 
 
@@ -19,18 +21,18 @@ const Nav = ({ setToggle }) => {
         <div>
             <nav className={navbarOpen ? "active-nav" : "nav"}>
                 <div className="nav-title">
-                <div className="nav-logo"> <img src={mainIcon} /></div>
-                <div className="menu-title"><a className="nav-link">Hello, Liqid</a></div>
+                {navbarOpen?<div className="nav-logo-open"> <img src={redLogo} /></div>:<div className="nav-logo"> <img src={mainIcon} /></div>}
+                <div className="menu-title"><NavLink to="/home" className="nav-link">Hello, Liqid</NavLink></div>
                 <div className="menu-title separator">|</div>
-                <div className="nav-item left-item"><a className="nav-link"><IoLogOutOutline className="icon" />Logout</a></div>
+                <div className="nav-item left-item"><NavLink to="/home" className="nav-link"><IoLogOutOutline className="icon" />Logout</NavLink></div>
                 </div>
               
                 <div className={navbarOpen ? "nav-menu active" : "nav-menu"}>
                 <ul className="nav-menu-right">
-                    <li className="nav-item right-item"><a className="nav-link"><IoLogOutOutline className="icon" />Logout</a></li>
-                    <li className="nav-item"><a className="nav-link"><AiOutlineHome className="icon" />Home</a></li>
-                    <li className="nav-item"><a className="nav-link"><BsPerson className="icon" />Profile</a></li>
-                    <li className="nav-item"><a className="nav-link"><FiSettings className="icon" />Settings</a></li>
+                    <li className="nav-item right-item"><NavLink to="/home" className="nav-link" ><IoLogOutOutline className="icon" />Logout</NavLink></li>
+                    <li className="nav-item"><NavLink to="/home" className="nav-link" ><AiOutlineHome className="icon" />Home</NavLink></li>
+                    <li className="nav-item"><NavLink to="/home" className="nav-link" ><BsPerson className="icon" />Profile</NavLink></li>
+                    <li className="nav-item"><NavLink to="/home" className="nav-link" ><FiSettings className="icon" />Settings</NavLink></li>
                 </ul>
                 <h2 >LIQID</h2>
                 </div>
