51486d7e742cb8959e72060c42c25fd62f21a91c
kalkidan <mesfinkalkidan@gmail.com>
2021-07-27 06:09:29 +0300

    edited menu style

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -15,26 +15,24 @@ const Nav = ({ setToggle }) => {
         setNavbarOpen(!navbarOpen)
 
     }
-    const closeMenu = () => {
-        setNavbarOpen(false)
-    }
     return (
         <div>
             <nav className={navbarOpen ? "active-nav" : "nav"}>
                 <div className="nav-title">
                 <div className="nav-logo"> <img src={mainIcon} /></div>
-                <div className="menu-title">Hello, Liqid</div>
+                <div className="menu-title"><a className="nav-link">Hello, Liqid</a></div>
                 <div className="menu-title separator">|</div>
+                <div className="nav-item left-item"><a className="nav-link"><IoLogOutOutline className="icon" />Logout</a></div>
                 </div>
               
                 <div className={navbarOpen ? "nav-menu active" : "nav-menu"}>
                 <ul className="nav-menu-right">
-                <li className="nav-item left-item"><a className="nav-link"><IoLogOutOutline className="icon" />Logout</a></li>
+                    <li className="nav-item right-item"><a className="nav-link"><IoLogOutOutline className="icon" />Logout</a></li>
                     <li className="nav-item"><a className="nav-link"><AiOutlineHome className="icon" />Home</a></li>
                     <li className="nav-item"><a className="nav-link"><BsPerson className="icon" />Profile</a></li>
                     <li className="nav-item"><a className="nav-link"><FiSettings className="icon" />Settings</a></li>
                 </ul>
-                <h2>LIQID</h2>
+                <h2 >LIQID</h2>
                 </div>
                 <div className="hamburger" onClick={handleToggle}>
                     {navbarOpen ? <AiOutlineClose className="toggle-icon" /> : <GiHamburgerMenu className="toggle-icon" />}
